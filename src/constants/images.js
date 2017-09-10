f1ec74e6b2962262d9f88a8b4c83e791abe55356
Jessica <jessica.dene.earley@gmail.com>
2017-09-10 16:09:17 -0700

    add the 9 speakers and removed those who didnt make it, add images

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,11 +1,13 @@
 export const alicia = require('../images/speakers/alicia.jpg')
-export const amrit = require('../images/speakers/amrit.jpg')
 export const caren = require('../images/speakers/caren.jpg')
+export const linda = require('../images/speakers/linda.jpg')
 export const minko = require('../images/speakers/minko.jpeg')
-export const sneha = require('../images/speakers/sneha.jpeg')
+export const rupali = require('../images/speakers/android.png')
+export const sneha = require('../images/speakers/sneha.jpg')
 export const vikram = require('../images/speakers/vikram.jpg')
 export const android = require('../images/speakers/android.png')
-export const speakers = { alicia, amrit, caren, minko, sneha, vikram, android }
+
+export const speakers = { alicia, caren, linda, minko, rupali, sneha, vikram, android }
 
 export const devFestBanner = require('../images/assets/devfestbanner.png')
 

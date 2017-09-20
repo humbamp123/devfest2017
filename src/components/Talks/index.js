db0ac105a8a0b38c9c32ef568e5fb64360f9b3c7
Queenie Ho <kuini.ho@gmail.com>
2017-09-20 01:05:10 -0700

    Talks updated

--- src/components/Talks/index.js
+++ src/components/Talks/index.js
@@ -4,23 +4,38 @@ import Maps from '../Maps';
 import Timer from '../Timer';
 import 'bulma/css/bulma.css';
 import c from './content.json';
+import { talksHero } from '../../constants/images';
 import './Talks.css'
 
 class Talks extends Component {
   render() {
     return (
-      <div className={'wrapper'}>
+      <div>
+      <div style={{backgroundImage: `url(${talksHero})`, backgroundSize: 'cover' , backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
         <section className={classNames('hero', 'is-medium', 'has-text-centered')}>
           <div className={'hero-body'}>
             <div className={'container'}>
               <h1 className={'talkTitle'}>{c.title}</h1>
-              <Timer customOptions={{endDate: '10/28/2017 9:00 AM'}} />
+              <h2 className={classNames('talkDate')}>{c.date}</h2>
+              <div className={classNames('container', 'blurbBody')}>
+                <p className={classNames('hackathonBlurb')}>{c.description}</p>
+              </div>
               <div className={classNames('button', 'registerButton', 'is-large')}>
                 <span>{c.register}</span>
               </div>
             </div>
           </div>
         </section>
+        <Timer customOptions={{endDate: '10/28/2017 9:00 AM', 'event': c.timeTill}} />
+      </div>
+        <div className={classNames('about')}>
+          <div className={'container'}>
+              <h2 className={classNames('aboutTitle')}>{c.aboutTitle}</h2>
+              <div className={classNames('container', 'aboutBody')}>
+                <p className={classNames('aboutBody')}>{c.aboutBody}</p>
+              </div>
+          </div>
+        </div>
         <Maps
           lat={37.787600}
           lng={-122.396600}
